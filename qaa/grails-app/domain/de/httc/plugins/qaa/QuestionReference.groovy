d8102139beefcdd6bd57bc231b0edd371b54a954
Stephan Tittel <stephan.tittel@httc.de>
2016-10-14 14:55:07 +0200

    downgrade to grails 2.5.3

--- qaa/grails-app/domain/de/httc/plugins/qaa/QuestionReference.groovy
+++ qaa/grails-app/domain/de/httc/plugins/qaa/QuestionReference.groovy
@@ -3,11 +3,10 @@ package de.httc.plugins.qaa
 import java.util.UUID
 
 class QuestionReference {
-    static mapping = {
-        //tablePerHierarchy false
-        id generator: "assigned"
-    }
+	static mapping = {
+		//tablePerHierarchy false
+		id generator: "assigned"
+	}
 
-    String id = UUID.randomUUID().toString()
+	String id = UUID.randomUUID().toString()
 }
-
