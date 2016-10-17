c933e323cac1039105f70f6961de2a3b5dfbde4e
Stephan Tittel <stephan.tittel@httc.de>
2016-10-17 11:40:59 +0200

    next dev version

--- repository/HttcRepositoryGrailsPlugin.groovy
+++ repository/HttcRepositoryGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class HttcRepositoryGrailsPlugin {
     // the plugin version
-	def version = /* @@VERSION@@ */ "2.0.2-SNAPSHOT" /* @@VERSION@@ */
+	def version = /* @@VERSION@@ */ "2.0.3-SNAPSHOT" /* @@VERSION@@ */
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
