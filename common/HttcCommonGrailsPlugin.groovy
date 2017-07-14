2b14748cda3f0bf068060425c5ae4436c2419861
Stephan Tittel <stephan.tittel@httc.de>
2017-07-14 15:08:41 +0200

    2.0.5-snapshot

--- common/HttcCommonGrailsPlugin.groovy
+++ common/HttcCommonGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class HttcCommonGrailsPlugin {
     // the plugin version
-    def version = /* @@VERSION@@ */ "2.0.4-SNAPSHOT" /* @@VERSION@@ */
+    def version = /* @@VERSION@@ */ "2.0.5-SNAPSHOT" /* @@VERSION@@ */
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
